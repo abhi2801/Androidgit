850d34fe225eee37d36f0727d7f4cffb0254db17
Abhishek Nayak <abhishek.nayak560@gmail.com>
2023-03-04 14:14:00 +0530

    Log added

--- app/src/main/java/com/example/application/MainActivity.kt
+++ app/src/main/java/com/example/application/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Toast.makeText(this, "first toast", Toast.LENGTH_SHORT).show()
+        Log.d("tag","Log added")
     }
 }
